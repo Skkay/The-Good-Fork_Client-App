cffa57ed7aaece8dc1cdebcdc995b31e5f51f9ec
Skkay <skkay.thomas@gmail.com>
2021-04-27 20:05:57 +0100

    Logout button for testing purpose

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, SafeAreaView, Text } from "react-native";
+import { AuthContext } from '../components/AuthContext';
 
 const HomeScreen = ({ navigation }) => {
+  const { signOut } = useContext(AuthContext);
+
   return (
     <SafeAreaView>
       <Text>Home screen</Text>
       <Button 
         title="Login"
         onPress={() => navigation.navigate('Login')}
+      />
+      <Button
+        title="Logout"
+        onPress={() => {signOut()}}
       />
     </SafeAreaView>
   );
