a810a920e9d7ff42dda53568961c2451abc18e75
Skkay <skkay.thomas@gmail.com>
2021-05-01 20:18:56 +0200

    Add tabs to 'OrderScreen'

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -1,12 +1,55 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const OrderScreen = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <SafeAreaView>
-      <Text>Order screen</Text>
+      <View style={styles.header}>
+        <TouchableOpacity style={[styles.headerButton, activeTab === 0 && styles.headerButtonActive]} onPress={() => setActiveTab(0)}>
+          <Text style={styles.headerButtonText}>Menus</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.headerButton, activeTab === 1 && styles.headerButtonActive]} onPress={() => setActiveTab(1)}>
+          <Text style={styles.headerButtonText}>Plats</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.headerButton, activeTab === 2 && styles.headerButtonActive]} onPress={() => setActiveTab(2)}>
+          <Text style={styles.headerButtonText}>Boissons</Text>
+        </TouchableOpacity>
+      </View>
+      {activeTab === 0 && (
+        <Text>Tab 1</Text>
+      )}
+      {activeTab === 1 && (
+        <Text>Tab 2</Text>
+      )}
+      {activeTab === 2 && (
+        <Text>Tab 3</Text>
+      )}
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row"
+  },
+  headerButton: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    backgroundColor: "#D1D1D1",
+  },
+  headerButtonActive: {
+    backgroundColor: "#FF8383",
+    borderBottomWidth: 3,
+    borderBottomColor: "#B74343",
+  },
+  headerButtonText: {
+    textAlign: "center",
+    fontSize: 18,
+    marginVertical: 10
+  }
+});
 
 export default OrderScreen;
