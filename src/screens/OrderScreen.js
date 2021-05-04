35dcb8930ee04e162ae902316363a96e46d97013
Skkay <skkay.thomas@gmail.com>
2021-05-04 20:01:08 +0200

    Move 'fetch()' method from 'MenuTab' to 'OrderScreen' to avoid re-fetch on tab change

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -1,10 +1,69 @@
-import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View, ActivityIndicator, Alert } from "react-native";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { AuthContext } from '../components/AuthContext';
 import MenuTab from "./order_tabs/MenuTab";
+
+const fetchToken = async() => {
+  let userToken = null;
+  try {
+    userToken = await AsyncStorage.getItem("userToken");
+  } catch (e) {
+    console.log(e);
+  } finally {
+    return userToken;
+  }
+}
 
 const OrderScreen = () => {
   const [activeTab, setActiveTab] = useState(0);
+  const { signOut } = useContext(AuthContext);
+  const [token, setToken] = useState(null);
+  const [dataMenu, setDataMenu] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchToken()
+      .then((token) => setToken(token))
+      .catch((err) => console.log(err));
+
+    if (!token) return;
+
+    const options = {
+      headers: {
+        'accept': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    }
+
+    // Menus fetching
+    fetch('http://192.168.1.18/3proj_api/public/api/menus', options)
+      .then((res) => {
+        if (!res.ok) {
+          if (res.status === 401) {
+            Alert.alert(
+              "Session expirée",
+              "Votre session a expiré, veuillez vous reconnecter.",
+              [{ text: "Ok", onPress: () => signOut() }]
+            );
+          }
+          throw Error(res.status);
+        }
+        return res.json();
+      })
+      .then((json) => setDataMenu(json))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+
+  }, [token]);
+
+  if (isLoading) {
+    return (
+      <ActivityIndicator size="large" color="#000000" />
+    );
+  }
 
   return (
     <SafeAreaView style={{ flex: 1}}>
@@ -20,7 +79,7 @@ const OrderScreen = () => {
         </TouchableOpacity>
       </View>
       {activeTab === 0 && (
-        <MenuTab />
+        <MenuTab data={dataMenu} />
       )}
       {activeTab === 1 && (
         <Text>Tab 2</Text>
