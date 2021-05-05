74a2fd2b9d7d8c39e69d83c76d2bb003bf983af8
Skkay <skkay.thomas@gmail.com>
2021-05-05 20:26:41 +0200

    Create 'FoodTab' view to display foods in 'OrderScreen'

---
+++ src/screens/order_tabs/FoodTab.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+import Item from "../../components/order/Item";
+
+const FoodTab = ({ data }) => {
+  const renderItem = ({ item }) => {
+    return (
+      <Item 
+        item={item}
+      />
+    );
+  }
+
+  return (
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+  );
+};
+
+export default FoodTab;
