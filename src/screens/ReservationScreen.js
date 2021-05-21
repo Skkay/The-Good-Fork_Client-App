432923a8fdf79540830fdc2efa562f7220e0110d
Skkay <skkay.thomas@gmail.com>
2021-05-21 19:50:52 +0200

    Fetch available tables using 'fetchAvailableTables'

--- src/screens/ReservationScreen.js
+++ src/screens/ReservationScreen.js
@@ -1,7 +1,46 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useEffect, useState, useContext } from "react";
+import { SafeAreaView, Text, ActivityIndicator } from "react-native";
+
+import { AuthContext } from '../components/AuthContext';
+import ExpiredSession from '../components/alert/ExpiredSession';
+import fetchToken from '../components/fetch/FetchToken';
+import fetchTokenValidity from '../components/fetch/FetchTokenValidity';
+import fetchAvailableTables from '../components/fetch/FetchAvailableTables';
 
 const ReservationScreen = () => {
+  const { signOut } = useContext(AuthContext);
+  const [token, setToken] = useState(null);
+  const [isValidToken, setValidToken] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchToken()
+      .then((token) => setToken(token))
+      .catch((err) => console.log(err));
+    if (!token) return;
+
+    // Checking token validity
+    fetchTokenValidity(token)
+      .then((res) => {
+        if (res.valid) {
+          setValidToken(true);
+        } else {
+          ExpiredSession(signOut);
+        }
+    })
+    if (!isValidToken) return;
+
+    fetchAvailableTables(token)
+      .then((res) => setData(res))
+      .finally(() => setLoading(false));
+  }, [token, isValidToken]);
+
+  if (isLoading) {
+    return (
+      <ActivityIndicator size="large" color="#000000" />
+    );
+  }
 
   return (
     <SafeAreaView>
