374471acbfb1691116545790810ad34e44be3237
Skkay <skkay.thomas@gmail.com>
2021-05-22 15:59:11 +0200

    Create 'PostReservation' to submit a table reservation to API

---
+++ src/components/fetch/PostReservation.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+import getFetchingOption from './options';
+
+const postReservation = (token, serviceId, tableId, date) => {
+  const options = getFetchingOption(token);
+
+  return axios({
+    method: "POST",
+    url: "http://192.168.1.18/3proj_api/public/api/reservations",
+    withCredentials: true,
+    data: {
+      serviceId: serviceId,
+      tableId: tableId,
+      date: date,
+    },
+    headers: {
+      'accept': options.headers.accept,
+      'Authorization': options.headers.Authorization
+    }
+  });
+}
+
+export default postReservation;
