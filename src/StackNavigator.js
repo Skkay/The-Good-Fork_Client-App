2c5adc801baa091dfb0f17f9986ae30e109fe5d8
Skkay <skkay.thomas@gmail.com>
2021-05-24 00:37:15 +0200

    Add "Bars" icon to 'Home' stack header

--- src/StackNavigator.js
+++ src/StackNavigator.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { StyleSheet, Pressable } from "react-native";
+
 import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from "./screens/HomeScreen";
@@ -9,13 +11,25 @@ import OrderScreen from "./screens/OrderScreen";
 import OrderTypeScreen from "./screens/OrderTypeScreen";
 import MyOrdersScreen from "./screens/MyOrdersScreen";
 import ReservationScreen from "./screens/ReservationScreen";
+import Icon from './components/svg/Icon';
 
 const Stack = createStackNavigator();
 
-const MainStackNavigator = () => {
+const MainStackNavigator = ({ navigation }) => {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} initialParams={{ toastType: "", toastExtra: {} }} />
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        initialParams={{ toastType: "", toastExtra: {} }} 
+        options={{ 
+          headerLeft: () => (
+            <Pressable style={styles.drawerIcon} onPress={() => navigation.openDrawer()}>
+              <Icon name="Bars" height="28" width="28" />
+            </Pressable>
+          ) 
+        }} 
+      />
       <Stack.Screen name="Menu" component={MenuScreen} />
       <Stack.Screen name="Food" component={FoodScreen} />
       <Stack.Screen name="Drink" component={DrinkScreen} />
@@ -33,5 +47,12 @@ const MyOrdersStackNavigator = () => {
     </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  drawerIcon: {
+    marginLeft: 15,
+    padding: 5
+  }
+});
 
 export { MainStackNavigator, MyOrdersStackNavigator };
